eefb86ddbf1482432f031135e7ad9803bc688e3c
Maarten Visscher <mail@maartenvisscher.nl>
2018-10-02 16:29:04 +0200

    Add boilerplate for ProgramController

--- src/classes/ProgramController.java
+++ src/classes/ProgramController.java
@@ -2,4 +2,11 @@ package classes;
 
 public class ProgramController {
 
+    public void run() {
+        //TODO
+    }
+
+    public static void main(String[] args) {
+        new ProgramController().run();
+    }
 }
