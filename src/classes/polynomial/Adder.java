81235d990fe48d86f7f929f289632421747bcfd3
Alex Thieme <38857430+Aaal-E@users.noreply.github.com>
2018-10-09 15:33:09 +0200

    created the adder

--- src/classes/polynomial/Adder.java
+++ src/classes/polynomial/Adder.java
@@ -1,15 +1,43 @@
 package classes.polynomial;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Adder {
 
     public List<Integer> add(List<Integer> f, List<Integer> g, int mod) {
-        return null;
+        List<Integer> result = new ArrayList<Integer>(); 
+        for (int i=0;i<(Math.min(f.size(),g.size()));i++){
+            result.add(f.get(i)+g.get(i));
+        }
+        if (f.size()>g.size()) {
+            for(int i = g.size(); i< f.size(); i++) {
+                result.add(f.get(i));
+            }
+        } else {
+            for(int i = f.size(); i< g.size(); i++) {
+                result.add(g.get(i));
+            }
+        }
+        //reduce?
+        return result;
     }
 
     public List<Integer> subtract(List<Integer> f, List<Integer> g, int mod) {
-        // Add f + (-g)
-        return null;
+        List<Integer> result = new ArrayList<Integer>(); 
+        for (int i=0;i<(Math.min(f.size(),g.size()));i++){
+            result.add(f.get(i)-g.get(i));
+        }
+        if (f.size()>g.size()) {
+            for(int i = g.size(); i< f.size(); i++) {
+                result.add(f.get(i));
+            }
+        } else {
+            for(int i = f.size(); i< g.size(); i++) {
+                result.add(-g.get(i));
+            }
+        }
+        //reduce?
+        return result;
     }
 }
