95b5ea4f05313b0b7bdbc8df449bd48de3414cd9
Alex Thieme <38857430+Aaal-E@users.noreply.github.com>
2018-10-09 16:31:09 +0200

    trying stuff

--- src/classes/polynomial/Divider.java
+++ src/classes/polynomial/Divider.java
@@ -20,9 +20,9 @@ public class Divider {
         if(g.equals(Polynomial.ZERO))
             return null;
         for(int i = 0; i<=f.size()-g.size(); i++) {
-            if(g.get(g.size()-1-i)==0)
-                q.add(0);
-            else
+//            if(g.get(g.size()-1)==0)
+//                q.add(0);
+//            else
                 q.add(0, temp.get(f.size()-i-1)/g.get(g.size()-1));
             for(int j = 0; j<i; j++)
                 temp2.add(0);
