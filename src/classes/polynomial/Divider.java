5d9f8647245f43cc21d30d5d4fdb4c0566008767
Alex Thieme <38857430+Aaal-E@users.noreply.github.com>
2018-10-09 15:59:51 +0200

    created divider, hopefully

--- src/classes/polynomial/Divider.java
+++ src/classes/polynomial/Divider.java
@@ -3,15 +3,28 @@ package classes.polynomial;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class Divider {
+    Adder adder = new Adder();
+    Multiplier mul = new Multiplier();
     public Result divide(List<Integer> f, List<Integer> g, int mod) {
 
         List<Integer> q = new ArrayList<>();
         List<Integer> r = new ArrayList<>();
-
+        List<Integer> temp = new ArrayList<>(f);
+        List<Integer> temp2 = new ArrayList<>();
         // Do algorithm
-
-
+        for(int i = 0; i<=f.size()-g.size(); i--) {
+            
+            q.add(0, temp.get(f.size()-i)/g.get(g.size()-i));
+            for(int j = 0; j<i; j++)
+                temp2.add(0);
+            temp2.add(q.get(0));
+            temp = adder.subtract(temp, mul.multiply(temp2, g, mod), mod);
+        }
+        r = adder.subtract(f, mul.multiply(q, g, mod), mod);
+        //reduce
         // Return result
         return new Result(q, r);
     }
