cbe81420390c27bf05fe85045088b0f749eb96aa
Alex Thieme <38857430+Aaal-E@users.noreply.github.com>
2018-10-09 15:40:42 +0200

    created multiply

--- src/classes/polynomial/Multiplier.java
+++ src/classes/polynomial/Multiplier.java
@@ -1,9 +1,21 @@
 package classes.polynomial;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Multiplier {
     public List<Integer> multiply(List<Integer> f, List<Integer> g, int mod) {
-        return null;
+        List<Integer> result = new ArrayList<Integer>(); 
+        for (int i=0;i<(f.size()+g.size());i++){
+            result.add(0);
+        }
+        
+        for(int i=0; i<f.size();i++) {
+            for(int j=0;j<g.size();j++) {
+                result.set(i+j, result.get(i+j)+f.get(i)*g.get(j));
+            }
+        }
+        //reduce
+        return result;
     }
 }
