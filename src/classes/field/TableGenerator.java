8658e7790ee89d2033b6d278bdd3bbb8cc5ca7ae
Thieme <a.j.thieme@student.tue.nl>
2018-10-16 15:42:40 +0200

    added addition and multiplication table generators

--- src/classes/field/TableGenerator.java
+++ src/classes/field/TableGenerator.java
@@ -1,5 +1,51 @@
 package classes.field;
 
-public class TableGenerator {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import classes.polynomial.Adder;
+import classes.polynomial.Multiplier;
+
+public class TableGenerator {
+    Adder adder = new Adder();
+    Multiplier multiplier = new Multiplier();
+    
+    List<List<List<Integer>>> createAddTable(List<Integer> characteristic, int mod){
+        List<List<List<Integer>>> result = new ArrayList<>();
+        List<List<Integer>> elements = new ArrayList<>();
+        for(int i=0; i<characteristic.size();i++) {
+            for(int j=0; j<mod; j++) {
+                elements.add(new ArrayList<Integer>(Arrays.asList(i, j)));
+            }
+        }
+        
+        for(int i=0;i<characteristic.size()*mod; i++) {
+            List<List<Integer>> temp = new ArrayList<>();
+            for(int j=0; j<characteristic.size()*mod; j++) {
+                temp.add(adder.add(elements.get(i), elements.get(j), mod));
+            }
+            result.add(temp);
+        }
+        return result;
+    }
+    
+    List<List<List<Integer>>> createMulTable(List<Integer> characteristic, int mod){
+        List<List<List<Integer>>> result = new ArrayList<>();
+        List<List<Integer>> elements = new ArrayList<>();
+        for(int i=0; i<characteristic.size();i++) {
+            for(int j=0; j<mod; j++) {
+                elements.add(new ArrayList<Integer>(Arrays.asList(i, j)));
+            }
+        }
+        
+        for(int i=0;i<characteristic.size()*mod; i++) {
+            List<List<Integer>> temp = new ArrayList<>();
+            for(int j=0; j<characteristic.size()*mod; j++) {
+                temp.add(multiplier.multiply(elements.get(i), elements.get(j), mod));
+            }
+            result.add(temp);
+        }
+        return result;
+    }
 }
